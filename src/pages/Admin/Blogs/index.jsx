65751654586760fc48eb6abaf2584c7303f9b8a6
Abdulloh Ibrohimov <109219970+ibrohimov0@users.noreply.github.com>
2023-02-25 17:39:12 +0500

    Admin panel

---
+++ src/pages/Admin/Blogs/index.jsx
@@ -0,0 +1,122 @@
+import './style.css';
+import { useEffect, useRef, useState } from 'react'
+import { DeleteBlog, GetBlog, PostBlog, PutBlog } from "../../../redux/blog";
+import { useSelector, useDispatch } from "react-redux";
+import { IMAGE_URL } from '../../../utils';
+import axios from 'axios';
+
+function Blog() {
+  const editor1 = useRef();
+  const Title = useRef();
+  const Text = useRef();
+  const Name = useRef();
+  const Date = useRef();
+  const [imgUpload, setImgUpload] = useState();
+  const [loading, setLoading] = useState();
+  const [BlogModal, SetBlogModal] = useState(false)
+  const [BlogModal1, SetBlogModal1] = useState(false)
+  const [BlogOverlay, SetBlogOverlay] = useState(false)
+  const dispatch = useDispatch()
+  const HandleFile = (e) => {
+    const formData = new FormData()
+    formData.append('file', e.target.files[0])
+    formData.append('upload_preset', 'images')
+    setLoading(true)
+    const postImage = async () => {
+        try {
+            const response = await axios.post(`${IMAGE_URL}`, formData)
+            setImgUpload(response?.data.secure_url)
+            setLoading(false)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    postImage()
+} 
+  const dataBlog = useSelector(state => state.blog)
+  useEffect(() => {
+      dispatch(GetBlog())
+  },[])
+  const blogDelete = (e) => {
+    dispatch(DeleteBlog(e.target.value))
+    dispatch(GetBlog())
+  }
+  const HandleSubmit = (e) => {
+    e.preventDefault();
+    const body = {
+        img: imgUpload,
+        title: Title.current.value,
+        text: Text.current.value,
+        name: Name.current.value,
+        date: Date.current.value
+    }
+    dispatch(PostBlog(body))
+    dispatch(GetBlog())
+    SetBlogModal(false)
+    SetBlogOverlay(false)
+    setImgUpload(null)
+}
+const HandleSubmit1 = (e) => {
+  e.preventDefault();
+  const body = {
+    img: imgUpload,
+    title: Title.current.value,
+    text: Text.current.value,
+    name: Name.current.value,
+    date: Date.current.value
+  }
+  console.log(body);
+  const id = editor1.current.value
+  dispatch(PutBlog({body, id}))
+  dispatch(GetBlog())
+  SetBlogModal1(false)
+  SetBlogOverlay(false)
+  setImgUpload(null)
+}
+  return (
+    <div className="Blog">
+      {BlogOverlay ? <div className="overlay" onClick={() => {SetBlogModal(false);SetBlogOverlay(false);SetBlogModal1(false)}}></div> :null}
+        {BlogModal ? <form onSubmit={HandleSubmit} className="projectModal">
+            <h3>Add Blog</h3>
+            {loading ? <p>Loading...</p> : <input type="file" onChange={HandleFile} />}
+            <input ref={Title} type="text" placeholder='Enter Blog Title' required/>
+            <input ref={Text} type="text" placeholder='Enter Blog Text' required/>
+            <input ref={Name} type="text" placeholder='Enter Blog Name' required/>
+            <input ref={Date} type="date" placeholder='Enter Blog Date' required/>
+            <button type="submit">Add </button>
+        </form> :null}
+        {BlogModal1 ? <form onSubmit={HandleSubmit1} className="projectModal">
+            <h3>Edit Blog</h3>
+            {loading ? <p>Loading...</p> : <input type="file" onChange={HandleFile} />}
+            <input ref={Title} type="text" placeholder='Enter Blog Title' required/>
+            <input ref={Text} type="text" placeholder='Enter Blog Text' required/>
+            <input ref={Name} type="text" placeholder='Enter Blog Name' required/>
+            <input ref={Date} type="date" placeholder='Enter Blog Date' required/>
+            <button type="submit">Edit</button>
+        </form> :null}
+        <div className="BlogNav">
+        <h1>Blogs</h1>
+        <button onClick={() => {SetBlogModal(true);SetBlogOverlay(true)}}>+ Add Blog</button>
+        </div>
+      <ul>
+      {dataBlog.getBlog.Success == true ? dataBlog.getBlog?.Data.map((elem, index) => 
+        <li key={index}>
+            <img src={elem.img} alt="" />
+            <h3>{elem.title}</h3>
+            <p>{elem.text}</p>
+            <div className="BlogDate">
+              <h5>{elem.name}</h5>
+              <h5>{elem.date}</h5>
+            </div>
+            <div className="AdBtnBox">
+                <button value={elem.id} onClick={blogDelete}><i class="fa-solid fa-trash"></i>Delete</button>
+                <button value={elem.id} ref={editor1} onClick={() => {SetBlogModal1(true);SetBlogOverlay(true);}}><i class="fa-solid fa-edit"></i>Edit</button>
+            </div>
+        </li>)
+        :dataBlog.getBlog.Loading == true ? <i class="loading fa-solid fa-spinner fa-spin-pulse"></i> : dataBlog.getBlog.Error == true ? <h3 className='Error'><i class="fa-solid fa-triangle-exclamation fa-fade"></i> Error 500</h3> : null}
+      </ul>
+    </div>
+  );
+}
+
+export default Blog;
---
+++ src/pages/Admin/Projects/index.jsx
@@ -0,0 +1,111 @@
+import './style.css';
+import { DeleteProject, GetProject, PostProject, PutProject } from "../../../redux/project";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useRef, useState } from 'react'
+import { IMAGE_URL } from '../../../utils';
+import axios from 'axios';
+
+function Projects() {
+    const name = useRef();
+    const editor = useRef();
+    const [projectModal, SetProjectModal] = useState(false)
+    const [projectModal1, SetProjectModal1] = useState(false)
+    const [projectOverlay, SetProjectOverlay] = useState(false)
+    const [imgUpload, setImgUpload] = useState();
+    const [loading, setLoading] = useState();
+    const dispatch = useDispatch()
+    const dataProject = useSelector(state => state.project)
+    const HandleFile = (e) => {
+        const formData = new FormData()
+        formData.append('file', e.target.files[0])
+        formData.append('upload_preset', 'images')
+        setLoading(true)
+        const postImage = async () => {
+            try {
+                const response = await axios.post(`${IMAGE_URL}`, formData)
+                setImgUpload(response?.data.secure_url)
+                setLoading(false)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        postImage()
+    } 
+    useEffect(() => {
+        dispatch(GetProject())
+    },[])
+    const projectDelete = (e) => {
+        dispatch(DeleteProject(e.target.value))
+        dispatch(GetProject())
+    }
+    const projectEdit = (e) => {
+        // dispatch(DeleteProject(e.target.value))
+        // dispatch(GetProject())
+        SetProjectModal1(true)
+        SetProjectOverlay(true)
+    }
+    const AddProject = () => {
+        SetProjectModal(true)
+        SetProjectOverlay(true)
+    }
+    const HandleSubmit = (e) => {
+        e.preventDefault();
+        const body = {
+            img: imgUpload,
+            name: name.current.value
+        }
+        dispatch(PostProject(body))
+        dispatch(GetProject())
+        SetProjectModal(false)
+        SetProjectOverlay(false)
+        setImgUpload(null)
+    }
+    const HandleSubmit1 = (e) => {
+        e.preventDefault();
+        const body = {
+            img: imgUpload,
+            name: name.current.value
+        }
+        const id = editor.current.value
+        dispatch(PutProject({body, id}))
+        dispatch(GetProject())
+        SetProjectModal1(false)
+        SetProjectOverlay(false)
+        setImgUpload(null)
+    }
+  return (
+    <div className="Projects">
+        {projectOverlay ? <div className="overlay" onClick={() => {SetProjectModal(false);SetProjectOverlay(false);SetProjectModal1(false)}}></div> :null}
+        {projectModal ? <form onSubmit={HandleSubmit} className="projectModal">
+            <h3>Add Project</h3>
+            <input type="text" ref={name} placeholder='Enter Project Name' required/>
+            {loading ? <p>Loading...</p> : <input type="file" onChange={HandleFile} />}
+            <button type="submit">Add </button>
+        </form> :null}
+        {projectModal1 ? <form onSubmit={HandleSubmit1} className="projectModal">
+            <h3>Edit Project</h3>
+            <input type="text" ref={name} placeholder='Enter Project Name' required/>
+            {loading ? <p>Loading...</p> : <input type="file" onChange={HandleFile} />}
+            <button type="submit">Edit</button>
+        </form> :null}
+        <div className="ProjectsNav">
+            <h1>Projects</h1>
+            <button onClick={AddProject}>+ Add Project</button>
+        </div>
+        <ul>
+            {dataProject.getProject.Success == true ? dataProject.getProject?.Data.map((elem, index) => 
+            <li key={index}>
+                <img src={elem.img} alt="img" />
+                <h3>{elem.name}</h3>
+                <div className="AdBtnBox">
+                    <button value={elem.id} onClick={projectDelete}><i class="fa-solid fa-trash"></i>Delete</button>
+                    <button value={elem.id} ref={editor} onClick={projectEdit}><i class="fa-solid fa-edit"></i>Edit</button>
+                </div>
+            </li>)
+            :dataProject.getProject.Loading == true ? <i class="loading fa-solid fa-spinner fa-spin-pulse"></i> : dataProject.getProject.Error == true ? <h3 className='Error'><i class="fa-solid fa-triangle-exclamation fa-fade"></i> Error 500</h3> : null}
+        </ul>
+    </div>
+  );
+}
+
+export default Projects;
