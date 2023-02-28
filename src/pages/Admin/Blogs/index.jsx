4d60c06dfd000db152e37fdba30d35c9c2d5f4da
Abdulloh Ibrohimov <109219970+ibrohimov0@users.noreply.github.com>
2023-02-28 19:01:19 +0500

    overlay edits

--- src/pages/Admin/Blogs/index.jsx
+++ src/pages/Admin/Blogs/index.jsx
@@ -15,7 +15,7 @@ function Blog() {
   const [loading, setLoading] = useState();
   const [BlogModal, SetBlogModal] = useState(false)
   const [BlogModal1, SetBlogModal1] = useState(false)
-  const [BlogOverlay, SetBlogOverlay] = useState(false)
+  const BlogOverlay = useRef()
   const dispatch = useDispatch()
   const HandleFile = (e) => {
     const formData = new FormData()
@@ -53,7 +53,7 @@ function Blog() {
     dispatch(PostBlog(body))
     dispatch(GetBlog())
     SetBlogModal(false)
-    SetBlogOverlay(false)
+    BlogOverlay.current.style.display = "none"
     setImgUpload(null)
 }
 const HandleSubmit1 = (e) => {
@@ -69,12 +69,12 @@ const HandleSubmit1 = (e) => {
   dispatch(PutBlog({body, id}))
   dispatch(GetBlog())
   SetBlogModal1(false)
-  SetBlogOverlay(false)
+  BlogOverlay.current.style.display = "none"
   setImgUpload(null)
 }
   return (
     <div className="Blog">
-      {BlogOverlay ? <div className="overlay" onClick={() => {SetBlogModal(false);SetBlogOverlay(false);SetBlogModal1(false)}}></div> :null}
+      <div className="overlay" ref={BlogOverlay} onClick={() => {SetBlogModal(false);BlogOverlay.current.style.display = "none";SetBlogModal1(false)}}></div>
         {BlogModal ? <form onSubmit={HandleSubmit} className="projectModal">
             <h3>Add Blog</h3>
             {loading ? <p>Loading...</p> : <input type="file" onChange={HandleFile} />}
@@ -95,7 +95,7 @@ const HandleSubmit1 = (e) => {
         </form> :null}
         <div className="BlogNav">
         <h1>Blogs</h1>
-        <button onClick={() => {SetBlogModal(true);SetBlogOverlay(true)}}>+ Add Blog</button>
+        <button onClick={() => {SetBlogModal(true);BlogOverlay.current.style.display="block"}}>+ Add Blog</button>
         </div>
       <ul>
       {dataBlog.getBlog.Success == true ? dataBlog.getBlog?.Data.map((elem, index) => 
@@ -109,7 +109,7 @@ const HandleSubmit1 = (e) => {
             </div>
             <div className="AdBtnBox">
                 <button value={elem.id} onClick={blogDelete}><i className="fa-solid fa-trash"></i>Delete</button>
-                <button value={elem.id} ref={editor1} onClick={() => {SetBlogModal1(true);SetBlogOverlay(true);}}><i className="fa-solid fa-edit"></i>Edit</button>
+                <button value={elem.id} ref={editor1} onClick={() => {SetBlogModal1(true);BlogOverlay.current.style.display="block";}}><i className="fa-solid fa-edit"></i>Edit</button>
             </div>
         </li>)
         :dataBlog.getBlog.Loading == true ? <i className="loading fa-solid fa-spinner fa-spin-pulse"></i> : dataBlog.getBlog.Error == true ? <h3 className='Error'><i className="fa-solid fa-triangle-exclamation fa-fade"></i> Error 500</h3> : null}
