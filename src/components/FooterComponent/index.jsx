d18fdbfb43b8f1b97a16a9d888a7db59359a8398
Abdurahmon Majidov <109199563+abdurahmonmajidov77@users.noreply.github.com>
2023-03-04 19:19:09 +0500

    df

--- src/components/FooterComponent/index.jsx
+++ src/components/FooterComponent/index.jsx
@@ -1,7 +1,11 @@
 import './style.css';
-import {NavLink} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 function FooterComponent() {
+    const navlink = useNavigate();
+    const HandleNav = () => {
+        navlink('admin')
+    }
   return (
     <div className="FooterComponent">
         <h1>Biz aloqadamiz</h1>
@@ -9,7 +13,7 @@ function FooterComponent() {
         <div className="FooterCompBox">
             <div className="FooterCompTexts">
                 <h2>+998 99 020 52 60</h2>
-                <h4><NavLink to='/admin'>Bizning manzilimiz:</NavLink></h4>
+                <h4 onClick={HandleNav}>Bizning manzilimiz:</h4>
                 <p>Tashkent shaxar, Olmazor tumani, Kichik halqa yo'li, 72 A uy.</p>
                 <div className="FooterCompIcons">
                     <i className='fa-brands fa-facebook'></i>
