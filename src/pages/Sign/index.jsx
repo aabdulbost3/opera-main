c63bc994caae425bc9661cab032e3f541117d762
Abdurahmon Majidov <109199563+abdurahmonmajidov77@users.noreply.github.com>
2023-03-04 19:29:41 +0500

    hi

---
+++ src/pages/Sign/index.jsx
@@ -0,0 +1,17 @@
+import './style.css';
+
+function SignPage() {
+  return (
+    <div className="SignPage">
+        <form className="SignModal">
+            <h1>Login in</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore ea omnis cumque reprehenderit dolores quo ullam magnam nam, numquam molestiae.</p>
+            <input type="text" placeholder='Ismingizni kiriting' className='ins'/>
+            <input type="text" placeholder='Parolingizni kiritng' className='ins'/>
+            <button>Sign in</button>
+        </form>
+    </div>
+  );
+}
+
+export default SignPage;
