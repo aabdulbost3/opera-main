77937195eeacd151b47666e85b2b13965ca46949
Abdurahmon Majidov <109199563+abdurahmonmajidov77@users.noreply.github.com>
2023-03-04 19:45:40 +0500

    hi

--- src/pages/Sign/index.jsx
+++ src/pages/Sign/index.jsx
@@ -1,14 +1,17 @@
 import './style.css';
 
 function SignPage() {
+    const HandleSubmit = () => {
+        
+    }
   return (
     <div className="SignPage">
-        <form className="SignModal">
+        <form onSubmit={HandleSubmit} className="SignModal">
             <h1>Log in</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore ea omnis cumque reprehenderit dolores quo ullam magnam nam, numquam molestiae.</p>
             <input type="text" placeholder='Ismingizni kiriting' className='ins'/>
             <input type="text" placeholder='Parolingizni kiritng' className='ins'/>
-            <button>Sign in</button>
+            <button type='submit'>Sign in</button>
         </form>
     </div>
   );
