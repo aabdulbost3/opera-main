76d6ed38e7bdf844012a47141f7af1a0846045d2
akmalxonov <saidamirxonakmalxonov@gmail.com>
2023-03-05 22:41:34 +0500

    edit translation json file

--- src/components/HeaderComponent/index.jsx
+++ src/components/HeaderComponent/index.jsx
@@ -7,7 +7,7 @@ const { t, i18n } = useTranslation();
         <div className="HeaderBox">
         <div className="HeaderInbox">
         <img src="https://res.cloudinary.com/dnuh1ejtz/image/upload/v1677329603/IMG_20230225_175225_045_jhpksg.jpg" alt="" />
-        <p>{t("Header.0")}</p>
+        <p>{t("header.0")}</p>
         </div>
         <img className='HeaderImg' src="https://res.cloudinary.com/dnuh1ejtz/image/upload/v1675857922/cdvfdefvedf_lqcync.webp" alt="" />
         </div>
