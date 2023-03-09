1aa2d0629a3ff1224a72d3d87828287594e4022a
Abdurahmon Majidov <109199563+abdurahmonmajidov77@users.noreply.github.com>
2023-03-09 17:23:04 +0500

    hi

---
+++ src/pages/More/index.jsx
@@ -0,0 +1,12 @@
+import FooterComponent from '../../components/FooterComponent/index'
+import Navbar from '../../components/NavbarComponent/index'
+import './style.css'
+
+function MorePage() {
+    return(
+        <div className="MorePage">
+            <Navbar/>
+            <FooterComponent/>
+        </div>
+    )
+}
