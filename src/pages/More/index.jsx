a19906bd803c5a03279f7a0df3e7417e28502218
Abdurahmon Majidov <109199563+abdurahmonmajidov77@users.noreply.github.com>
2023-03-09 20:02:30 +0500

    hi

--- src/pages/More/index.jsx
+++ src/pages/More/index.jsx
@@ -1,13 +1,18 @@
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import FooterComponent from '../../components/FooterComponent/index'
 import Navbar from '../../components/NavbarComponent/index'
 import './style.css'
+import {API_URL} from '../../utils/index'
+import axios from 'axios'
 
 function MorePage() {
+    const [data, setData] = useState([])
     const navigate = useNavigate()
     const HandleItem = () => {
         navigate('/')
     }
+    
     return(
         <div className="MorePage">
             <Navbar/>
