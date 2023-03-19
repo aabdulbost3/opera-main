e4ba7646e63d0aad892f8e9f18c31d033c0a0806
Abdurahmon Majidov <109199563+abdurahmonmajidov77@users.noreply.github.com>
2023-03-19 17:46:58 +0500

    ok

--- src/components/FooterComponent/index.jsx
+++ src/components/FooterComponent/index.jsx
@@ -31,51 +31,6 @@ function FooterComponent() {
         navlink('admin')
     }
     const Post = (e)=>{
-<<<<<<< HEAD
-        e.preventDefault()
-        const body = {
-            title: MesName.current.value,
-            phone: MesPhone.current.value,
-            text: MesMessage.current.value
-        }
-        dispatch(PostMessage(body))
-        MesName.current.value = null
-        MesPhone.current.value = null
-        MesMessage.current.value = null
-    }
-    return(
-        <>
-        <div className="FooterComponent" id="2">
-        <h1>{t("Footer.0")}</h1>
-        <p>{t("Footer.1")}</p>
-        <div className="FooterCompBox">
-            <div className="FooterCompTexts">
-                <h2>+998 99 020 52 60</h2>
-                <h4 onClick={HandleNav}>{t("Footer.2")}:</h4>
-                <p>{t("Footer.3")}</p>
-                <div className="FooterCompIcons">
-                    <i className='fa-brands fa-facebook'></i>
-                    <i className='fa-brands fa-instagram'></i>
-                    <i className='fa-brands fa-telegram'></i>
-                </div>
-                <form className="FooterCompInbox" onSubmit={Post}>
-                    <input type="text" ref={MesName} placeholder={t("Message.0")} className='ins' />
-                    <input type="tel" ref={MesPhone} placeholder='+998 (__) _ _ _-_ _-_ _' className='ins' />
-                    <textarea ref={MesMessage} className='ins' required placeholder={t("Message.1")}></textarea>
-                    <button type='submit'>{t("Footer.4")}</button>
-                </form>
-            </div>
-            <form className="FooterCompInbox" onSubmit={Post}>
-                <input type="text" ref={MesName} placeholder={t("Message.0")} className='ins'/>
-                <input type="tel" ref={MesPhone} placeholder='+998 (__) _ _ _-_ _-_ _' className='ins'/>
-                <textarea ref={MesMessage} className='ins' required placeholder={t("Message.1")}></textarea>
-                <button type="submit">{t("Footer.4")}</button>
-            </form>
-            </div>
-        </div>
-        </>
-    );
-=======
     e.preventDefault()
     sendtelegram();
     const body = {
@@ -128,7 +83,6 @@ function FooterComponent() {
     </div>
 </div>
   );
->>>>>>> eebae0204ce5cbf79752e3097fb4f3fd5337287c
 }
 
 export default FooterComponent
